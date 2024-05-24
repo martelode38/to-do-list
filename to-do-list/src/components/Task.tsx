485ad30ce1181df6da423dbf2fc22cca7a8b9df3
martelode38 <marteniers@gmail.com>
2024-05-24 04:10:20 -0300

    .

--- to-do-list/src/components/Task.tsx
+++ to-do-list/src/components/Task.tsx
@@ -1,23 +1,38 @@
 import { Trash } from 'phosphor-react';
 import styles from './Task.module.css';
+import { useState } from 'react';
+import vector from '../assents/Vector.svg';
 
 interface taskProps{
     content: string;
     deleteTask: (task: string) => void;
-    
+
 }
 
 
 export function Task({content, deleteTask}: taskProps){
     
+    const [click, setClick] = useState(false);
+    
+    
+    function botaoConcluir(){
+        setClick(!click); 
+    }
+
     function deleteOneTask(){
         deleteTask(content);
     }
 
+
+
     return(
         <div className={styles.task}>
-            <div className={styles.caixaDeMarcar}>
-
+            
+            <div
+            className={click != true ? styles['caixaDeMarcar'] : styles['caixaDeMarcarOn']}
+            onClick={botaoConcluir}
+            >
+                <img src={vector} alt="vector" />
             </div>
 
             <p>{content}</p>
